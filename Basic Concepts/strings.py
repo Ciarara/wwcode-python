699d58fa1799e5e2910a378de2badba60865fd90
Ciara Gale Bautista <17606298+ciarabautista@users.noreply.github.com>
2020-07-19 12:51:41 +0800

    Add files via upload

---
+++ Basic Concepts/strings.py
@@ -0,0 +1,9 @@
+#string - a series of letters, numbers, symbols enclosed in single quote (') or double quote (").
+
+greeting = "WWC is awesome! "
+title = 'StringPaMore'
+hash_symbol = "#"
+hashtag = hash_symbol + title
+rating = str(5)
+print("{} {} stars for them! {}" .format(greeting, rating, hashtag))
+#print(f" {greeting} {rating} stars for them! {hashtag}") #For versions Python 3.6
