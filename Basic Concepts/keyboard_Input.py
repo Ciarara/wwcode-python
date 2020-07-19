1aae6cf725ecb92ff3ab0cc21d6796d98c80b72e
Ciara Gale Bautista <17606298+ciarabautista@users.noreply.github.com>
2020-07-19 09:49:34 +0800

    Update keyboard_Input.py
    Simpy asks a user for an input.

--- Basic Concepts/keyboard_Input.py
+++ Basic Concepts/keyboard_Input.py
@@ -1,3 +1,10 @@
+#Keyboard input will stop the program flow and asks the user for an input.
+#Keyboard input has the ff. syntax: input()
+#It can have an optional prompt string: input('Enter your age:')
+
+
+
+
 name = input('Your name: ')
 tweet = input('Tweet your day: ')
 print('{} tweeted {}' .format(name, tweet)) #String formatting replaces {} with variable values
