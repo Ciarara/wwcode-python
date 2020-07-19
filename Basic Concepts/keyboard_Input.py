dc702d7b4d70e1d46fce67f7266209edbf86b04c
Ciara Gale Bautista <17606298+ciarabautista@users.noreply.github.com>
2020-07-19 09:48:05 +0800

    Add files via upload

---
+++ Basic Concepts/keyboard_Input.py
@@ -0,0 +1,4 @@
+name = input('Your name: ')
+tweet = input('Tweet your day: ')
+print('{} tweeted {}' .format(name, tweet)) #String formatting replaces {} with variable values
+#print(f'{name} tweeted {tweet}') # New string formatting added on Python 3.6
