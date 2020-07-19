f549920598f077105c0308358fe66fd8a8646434
Ciara Gale Bautista <17606298+ciarabautista@users.noreply.github.com>
2020-07-19 09:38:37 +0800

    Add files via upload

---
+++ Basic Concepts/arithmetic_operations.py
@@ -0,0 +1,33 @@
+#It would be best if you try using the Python shell for the following codes 
+
+#Addition 
+a = 20
+b = 10
+sum = a + b
+print(sum)
+
+20 + 10 
+
+#Subtraction 
+20 - 10
+
+#Multiplication 
+20 * 10 
+
+#Division 
+20 / 10 
+
+#Floor Division 
+
+9 // 2
+10 // 3 
+
+#Exponent 
+
+4 ** 2
+
+#Modulo 
+
+10 % 3
+99 % 33
+
