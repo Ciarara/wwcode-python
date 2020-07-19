e093866d0565aba19c8a44c70e2a4753911cc34f
Ciara Gale Bautista <17606298+ciarabautista@users.noreply.github.com>
2020-07-19 11:32:46 +0800

    Add files via upload
    this is a test file for arithmetic operations from https://medium.com/programminginpython-com/python-program-for-performing-arithmetic-operations-cae64e6e7781

---
+++ Basic Concepts/arith1.py
@@ -0,0 +1,20 @@
+num1 = int(input('Enter the First number: '))
+num2 = int(input('Enter Second number: '))
+add = num1 + num2 
+diff = num1 - num2
+mul = num1 * num2 
+div = num1 / num2 
+floor_div = num1 // num2 
+power = num1 ** num2 
+modulus = num1 % num2
+print('Sum of' , num1, 'and' , num2, 'is : ' , add)
+print('Difference of' , num1, 'and' , num2, 'is : ' , diff)
+print('Product of' , num1, 'and' , num2, 'is : ' , mul)
+print('Division of' , num1, 'and' , num2, 'is : ' , div)
+print('Floor Division of ', num1, 'and' , num2 , 'is : ', floor_div)
+print('Exponent' , num1, 'and' , num2, 'is : ' , power)
+print('Modulus ', num1, 'and' , num2 , 'is : ', modulus)
+
+
+
+
